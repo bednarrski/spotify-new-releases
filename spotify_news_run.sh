a217acd41cf8689b8f9d31cbf58e8b38601efac2
bednarrski <bednarrski@gmail.com>
2018-07-23 11:18:36 +0200

    "Update at 20180723_091836"

--- spotify_news_run.sh
+++ spotify_news_run.sh
@@ -1,4 +1,12 @@
 rm -f ~/Logs/spotiffy_news_log.older
 mv ~/Logs/spotiffy_news_log.old ~/Logs/spotiffy_news_log.older
 mv ~/Logs/spotiffy_news_log.txt ~/Logs/spotiffy_news_log.old
-~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1
+if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1 ; then
+    echo "SUCCESS!" >> ~/Logs/spotiffy_news_log.txt 2>&1
+else
+	if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1 ; then
+        echo "SUCCESS (2nd RUN NEEDED)!" >> ~/Logs/spotiffy_news_log.txt 2>&1
+    else
+	    echo "FAILED 2 TIMES!" >> ~/Logs/spotiffy_news_log.txt 2>&1
+    fi
+fi
