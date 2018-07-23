48a8ff95be7c7a8af1e1b8d3e583a7f04cde647c
bednarrski <bednarrski@gmail.com>
2018-07-23 11:25:33 +0200

    "Update at 20180723_092533"

--- spotify_news_run.sh
+++ spotify_news_run.sh
@@ -4,9 +4,14 @@ mv ~/Logs/spotiffy_news_log.txt ~/Logs/spotiffy_news_log.old
 if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1 ; then
     echo "SUCCESS!" >> ~/Logs/spotiffy_news_log.txt 2>&1
 else
-	if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1 ; then
+    if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1 ; then
         echo "SUCCESS (2nd RUN NEEDED)!" >> ~/Logs/spotiffy_news_log.txt 2>&1
     else
-	    echo "FAILED 2 TIMES!" >> ~/Logs/spotiffy_news_log.txt 2>&1
+        if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1 ; then
+            echo "SUCCESS (3rd RUN NEEDED)!" >> ~/Logs/spotiffy_news_log.txt 2>&1
+        else
+            echo "FAILED 3 TIMES!" >> ~/Logs/spotiffy_news_log.txt 2>&1
+        fi
     fi
 fi
+echo $(date) >> ~/Logs/spotiffy_news_log.txt 2>&1
