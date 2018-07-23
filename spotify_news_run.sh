2731e2a855cb7b937f99883b90b25b0a9ccbaea4
bednarrski <bednarrski@gmail.com>
2018-07-23 08:12:13 +0200

    "Update at 20180723_061213"

--- spotify_news_run.sh
+++ spotify_news_run.sh
@@ -1,4 +1,4 @@
 rm -f ~/Logs/spotiffy_news_log.older
 mv ~/Logs/spotiffy_news_log.old ~/Logs/spotiffy_news_log.older
 mv ~/Logs/spotiffy_news_log.txt ~/Logs/spotiffy_news_log.old
-~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases.py >> ~/Logs/spotiffy_news_log.txt 2>&1
+~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1
