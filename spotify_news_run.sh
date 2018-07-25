0ea14f30737523859ab8e43a964bfa84bfe4d4f9
bednarrski <bednarrski@gmail.com>
2018-07-25 09:30:32 +0200

    "Update at 20180725_073032"

--- spotify_news_run.sh
+++ spotify_news_run.sh
@@ -1,17 +1,17 @@
-rm -f ~/Logs/spotiffy_news_log.older
-mv ~/Logs/spotiffy_news_log.old ~/Logs/spotiffy_news_log.older
-mv ~/Logs/spotiffy_news_log.txt ~/Logs/spotiffy_news_log.old
-if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1 ; then
-    echo "SUCCESS!" >> ~/Logs/spotiffy_news_log.txt 2>&1
+rm -f ~/Logs/spotify_news_log.older
+mv ~/Logs/spotify_news_log.old ~/Logs/spotify_news_log.older
+mv ~/Logs/spotify_news_log.txt ~/Logs/spotify_news_log.old
+if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
+    echo "SUCCESS!" >> ~/Logs/spotify_news_log.txt 2>&1
 else
-    if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1 ; then
-        echo "SUCCESS (2nd RUN NEEDED)!" >> ~/Logs/spotiffy_news_log.txt 2>&1
+    if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
+        echo "SUCCESS (2nd RUN NEEDED)!" >> ~/Logs/spotify_news_log.txt 2>&1
     else
-        if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotiffy_news_log.txt 2>&1 ; then
-            echo "SUCCESS (3rd RUN NEEDED)!" >> ~/Logs/spotiffy_news_log.txt 2>&1
+        if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
+            echo "SUCCESS (3rd RUN NEEDED)!" >> ~/Logs/spotify_news_log.txt 2>&1
         else
-            echo "FAILED 3 TIMES!" >> ~/Logs/spotiffy_news_log.txt 2>&1
+            echo "FAILED 3 TIMES!" >> ~/Logs/spotify_news_log.txt 2>&1
         fi
     fi
 fi
-echo $(date) >> ~/Logs/spotiffy_news_log.txt 2>&1
+echo $(date) >> ~/Logs/spotify_news_log.txt 2>&1
