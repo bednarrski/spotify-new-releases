6246ac962fc835880f133d0d85b1434150d77289
bednarrski <bednarrski@gmail.com>
2018-09-24 09:45:51 +0200

    "Update at 20180924_074551"

--- spotify_news_run.sh
+++ spotify_news_run.sh
@@ -2,7 +2,8 @@
 
 filename='/Users/bednar/Logs/spotify_news_log.txt'
 
-if [[ $(find "$filename" -mmin +$((60*24*2)) -print) ]]; then
+#if [[ $(find "$filename" -mmin +$((60*24*2)) -print) ]]; then
+if [[ $(find "$filename" -mmin +$((2)) -print) ]]; then
 	echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 	if [ $? -eq 0 ]; then
 
@@ -19,7 +20,15 @@ if [[ $(find "$filename" -mmin +$((60*24*2)) -print) ]]; then
 		        if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
 		            echo "SUCCESS (3rd RUN NEEDED)!" >> ~/Logs/spotify_news_log.txt 2>&1
 		        else
-		            echo "FAILED 3 TIMES!" >> ~/Logs/spotify_news_log.txt 2>&1
+			        if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
+			            echo "SUCCESS (3rd RUN NEEDED)!" >> ~/Logs/spotify_news_log.txt 2>&1
+			        else
+				        if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
+				            echo "SUCCESS (3rd RUN NEEDED)!" >> ~/Logs/spotify_news_log.txt 2>&1
+				        else
+				            echo "FAILED 5 TIMES!" >> ~/Logs/spotify_news_log.txt 2>&1
+				        fi
+			        fi
 		        fi
 		    fi
 		fi
