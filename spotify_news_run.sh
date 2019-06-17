38e14c34d266c3a2173278ceb5ade57bff2e5855
Piotr Bednarski <bednar@Piotrs-MacBook-Pro.local>
2019-06-17 09:16:13 +0200

    "Update at 20190617_071613"

--- spotify_news_run.sh
+++ spotify_news_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-filename='/Users/pib/Logs/spotify_news_log.txt'
+filename='/Users/bednar/Logs/spotify_news_log.txt'
 
 
-if [[ $(find "$filename" -mmin +$((2*24*60)) -print) ]]; then
+if [[ $(find "$filename" -mmin +$((1)) -print) ]]; then
 	echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 	if [ $? -eq 0 ]; then
 
@@ -11,19 +11,19 @@ if [[ $(find "$filename" -mmin +$((2*24*60)) -print) ]]; then
 		mv ~/Logs/spotify_news_log.old ~/Logs/spotify_news_log.older
 		mv ~/Logs/spotify_news_log.txt ~/Logs/spotify_news_log.old
 
-		if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
+		if /Users/bednar/anaconda3/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
 		    echo "SUCCESS!" >> ~/Logs/spotify_news_log.txt 2>&1
 		else
-		    if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
+		    if /Users/bednar/anaconda3/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
 		        echo "SUCCESS (2nd RUN NEEDED)!" >> ~/Logs/spotify_news_log.txt 2>&1
 		    else
-		        if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
+		        if /Users/bednar/anaconda3/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
 		            echo "SUCCESS (3rd RUN NEEDED)!" >> ~/Logs/spotify_news_log.txt 2>&1
 		        else
-			        if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
+			        if /Users/bednar/anaconda3/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
 			            echo "SUCCESS (4th RUN NEEDED)!" >> ~/Logs/spotify_news_log.txt 2>&1
 			        else
-				        if ~/anaconda3/envs/py36/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
+				        if /Users/bednar/anaconda3/bin/python ~/Repositories/spotify-new-releases/spotify-new-releases2.py >> ~/Logs/spotify_news_log.txt 2>&1 ; then
 				            echo "SUCCESS (5th RUN NEEDED)!" >> ~/Logs/spotify_news_log.txt 2>&1
 				        else
 				            echo "FAILED 5 TIMES!" >> ~/Logs/spotify_news_log.txt 2>&1
